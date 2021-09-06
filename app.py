4eae50ac9a05191ecd7f2b8d1b5016536281131b
Dennis Fenchenko <denisfench@gmail.com>
2021-09-06 13:01:00 -0400

    adding requirenments file and seed

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
 import urllib.request, urllib.parse, urllib.error
 import ssl
+import requests
 from bs4 import BeautifulSoup
+from queue import PriorityQueue
+from googlesearch import search
+
+# obtaining a seed set of pages from Google based on query 
+query = input("Please enter a search query ")
+seed = search(query, stop=10)
+urls = PriorityQueue()
+urls.put((1, "https://en.wikipedia.org/wiki/Alan_Turing"))
+
+for webpage in seed:
+    print(webpage)
 
 # ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-html = urllib.request.urlopen("https://en.wikipedia.org/wiki/Alan_Turing").read()
+html = urllib.request.urlopen("https://en.wikipedia.org/wiki/Alan_Turing", context=ctx).read()
 soup = BeautifulSoup(html, 'html.parser')
 
 tags = soup('a')
-for tag in tags:
-    print(tag.get('href', None))
+# for tag in tags:
+#     # print(tag.get('href', None))
 
 # for line in fhandle:
-#     print(line.decode().strip())
+    # print(line.decode().strip())
