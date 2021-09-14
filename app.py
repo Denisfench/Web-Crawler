18b6a22d9db68e50feb0a21c7b64cea50e0f0ca5
Dennis Fenchenko <denisfench@gmail.com>
2021-09-14 00:23:16 -0400

    Adding concurrency

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from googlesearch import search
 from urllib.parse import urljoin, urlparse
 from urllib import robotparser
 from url_normalize import url_normalize
+from threading import Thread
 
 # obtaining a seed set of pages from Google based on query
 query = input("Please enter a search query ")
@@ -24,6 +25,8 @@ NOVELTY_IDX = 2
 IMPORTANCE_IDX = 3
 DEBUG = False
 CRAWLER_NAME = "NYU_Crawler"
+THREAD_POOL_SIZE = 10
+
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'}
 
 # print("seed start")
@@ -70,6 +73,7 @@ def update_score(urls_visited, domain_url, novelty_weight, importance_weight, cr
             urls_visited[domain_url] = list((priority, pages_crawled, curr_novelty, new_importance))
 
 
+# can be parallelized
 def crawler_allowed(root_url, crawled_url):
     rp = robotparser.RobotFileParser()
     url_scheme = urlparse(crawled_url).scheme + "://"
@@ -117,57 +121,71 @@ for webpage in seed:
     update_score(urls_visited, webpage_domain, 0.5, 0.5)
 
 start_time = time.time()
-seconds = 4
-while not urls.empty():
-    count += 1
-    curr_time = time.time()
-    print("visited count is ", count)
-    if DEBUG:
+seconds = 10
+# has to be parallelized
+def crawl_pages():
+    count = 0
+    while not urls.empty():
+        count += 1
+        curr_time = time.time()
         print("visited count is ", count)
-        if curr_time - start_time > seconds:
-            break
-    curr_url = urls.get()
-    curr_rank = curr_url[0]
-    curr_url = curr_url[1]
-    print("curr url is ", curr_url)
-    curr_url_domain = urlparse(curr_url).netloc
-    if not crawler_allowed(curr_url_domain, curr_url): continue
-    updated_rank = urls_visited[curr_url_domain][RANK_IDX]
-    if DEBUG:
-        print("updated rank is ", updated_rank)
-        print("curr rank is", curr_rank)
-    if updated_rank != - curr_rank:
-        urls.put(tuple((- updated_rank, curr_url)))
-        continue
-    try:
-        response = requests.get(curr_url, timeout=10, headers=headers)
-    except:
-        pass
-    if response.status_code == 200:
-        # content_length = response.headers['content-length'] not neccessarily present
-        content_length = len(response.content)
-        update_score(urls_visited, curr_url_domain, 0.5, 0.5, crawling=True, see_again=False)
-        get_log(count, curr_url, urls_visited[curr_url_domain][RANK_IDX], curr_time, content_length)
-        html_doc = response.text
-        soup = BeautifulSoup(html_doc, 'html.parser')
-        url_link_count = 0
-        for url_link in soup.find_all('a', href=True):
-            url_link_count += 1
-            if DEBUG:
-                print("url link count is ", url_link_count)
-            abs_url = urljoin(curr_url, url_link['href'])
-            # normilize url
-            curr_url = url_normalize(abs_url)
-            domain_url = urlparse(curr_url).netloc
-            if domain_url in urls_visited:
-                update_score(urls_visited, domain_url, 0.5, 0.5)
-            else:
-                update_score(urls_visited, domain_url, 0.5, 0.5)
-                urls.put(tuple((- urls_visited[domain_url][RANK_IDX], curr_url)))
+        if DEBUG:
+            print("visited count is ", count)
+            if curr_time - start_time > seconds:
+                break
+        curr_url = urls.get()
+        curr_rank = curr_url[0]
+        curr_url = curr_url[1]
+        print("curr url is ", curr_url)
+        curr_url_domain = urlparse(curr_url).netloc
+        if not crawler_allowed(curr_url_domain, curr_url): continue
+        updated_rank = urls_visited[curr_url_domain][RANK_IDX]
+        if DEBUG:
+            print("updated rank is ", updated_rank)
+            print("curr rank is", curr_rank)
+        if updated_rank != - curr_rank:
+            urls.put(tuple((- updated_rank, curr_url)))
+            continue
+        try:
+            response = requests.get(curr_url, timeout=10, headers=headers)
+        except:
+            pass
+        if response.status_code == 200:
+            # content_length = response.headers['content-length'] not neccessarily present
+            content_length = len(response.content)
+            update_score(urls_visited, curr_url_domain, 0.5, 0.5, crawling=True, see_again=False)
+            get_log(count, curr_url, urls_visited[curr_url_domain][RANK_IDX], curr_time, content_length)
+            html_doc = response.text
+            soup = BeautifulSoup(html_doc, 'html.parser')
+            url_link_count = 0
+            for url_link in soup.find_all('a', href=True):
+                url_link_count += 1
+                if DEBUG:
+                    print("url link count is ", url_link_count)
+                abs_url = urljoin(curr_url, url_link['href'])
+                # normilize url
+                curr_url = url_normalize(abs_url)
+                domain_url = urlparse(curr_url).netloc
+                if domain_url in urls_visited:
+                    update_score(urls_visited, domain_url, 0.5, 0.5)
+                else:
+                    update_score(urls_visited, domain_url, 0.5, 0.5)
+                    urls.put(tuple((- urls_visited[domain_url][RANK_IDX], curr_url)))
 
 
-# print("The urls are", len(urls_visited))
-# for url in urls_visited.items():
-#     print(url)
+def main():
+    threads = [
+        Thread(target=crawl_pages)
+        for _ in range(THREAD_POOL_SIZE)
+    ]
+    for thread in threads:
+        thread.start()
+    while threads:
+        threads.pop().join()
 
-# print_urls()
+if __name__ == "__main__":
+    started = time.time()
+    main()
+    elapsed = time.time() - started
+    print()
+    print("time elapsed: {:.2f}s".format(elapsed))
